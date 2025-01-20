ee273944fb9a3fc3a27ac1c7d5815d7b3e4984d6
armag3ddon <armag3ddon@web.de>
2025-01-20 11:56:22 +0100

    Implement basic config management.

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
+from config import Config
 from flask_bootstrap import Bootstrap
 
 # Load modules
 bootstrap = Bootstrap()
 
 # Create the app, called in 
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__)
+    app.config.from_object(Config)
 
     # Initiate modules
     bootstrap.init_app(app)
