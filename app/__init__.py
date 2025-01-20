19c3b9d96ee731442b5e135a4de428505988ec27
armag3ddon <armag3ddon@web.de>
2025-01-20 01:00:31 +0100

    Add basic error handling

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,18 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
-app = Flask(__name__)
+# Load modules
+bootstrap = Bootstrap()
 
-from app import routes
+# Create the app, called in 
+def create_app():
+    app = Flask(__name__)
+
+    # Initiate modules
+    bootstrap.init_app(app)
+
+    # Load error handling blueprint
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
+    return app
