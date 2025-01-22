f12c2649342384848512b236e4df52d30a56a9a7
armag3ddon <armag3ddon@web.de>
2025-01-22 18:37:47 +0100

    Error messages for input fields.

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import InputRequired
 from flask_babel import _
 
 class LoginForm(FlaskForm):
-    username = StringField(_('Benutzername', validators=[DataRequired()]))
-    password = PasswordField(_('Passwort'), validators=[DataRequired()])
+    username = StringField(_('Benutzername', validators=[InputRequired(_('Feld muss ausgefüllt werden'))]))
+    password = PasswordField(_('Passwort'), validators=[InputRequired(_('Feld muss ausgefüllt werden'))])
     remember_me = BooleanField(_('Eingeloggt bleiben'))
     submit = SubmitField('Einloggen')
