6ef88d95037aa6643a3da9f3387b2cc4bdcc9183
armag3ddon <armag3ddon@web.de>
2025-01-23 23:26:03 +0100

    Add rights management model to database.

--- app/models.py
+++ app/models.py
@@ -6,6 +6,12 @@ import sqlalchemy.orm as so
 from app import db, login
 from flask_login import UserMixin
 
+# -------------------------------------
+# Definition of database models
+# Models are being mapped to database tables
+# -------------------------------------
+
+# User account
 class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(128), index=True, unique=True)
@@ -14,6 +20,8 @@ class User(UserMixin, db.Model):
     email: so.Mapped[str] = so.mapped_column(sa.String(256), index=True, unique=True)
     phone: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+
+    rights: so.Mapped['Right'] = so.relationship(back_populates='user')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -27,3 +35,16 @@ class User(UserMixin, db.Model):
 @login.user_loader
 def load_user(id):
     return db.session.get(User, int(id))
+
+# Rights management
+class Right(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
+    is_admin: so.Mapped[bool] = so.mapped_column(default=False)
+    login_allowed: so.Mapped[bool] = so.mapped_column(default=True)
+    create_calendar_entry: so.Mapped[bool] = so.mapped_column(default=False)
+
+    user: so.Mapped[User] = so.relationship(back_populates='rights')
+
+    def __repr__(self):
+        return '<Right by {}, admin: {}>'.format(self.user_id, self.is_admin)
