29bc600b734f5753e1e538fe0c832bf73ccc8439
armag3ddon <armag3ddon@web.de>
2025-01-26 16:49:35 +0100

    Add CalendarEntry to the shell.

--- planetarium.py
+++ planetarium.py
@@ -1,11 +1,11 @@
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import create_app, db
-from app.models import User, Right
+from app.models import User, Right, CalendarEntry
 
 app = create_app()
 
 # Populate the "flask shell" command with the database connection and database models
 @app.shell_context_processor
 def make_shell_context():
-    return { 'sa': sa, 'so': so, 'db': db, 'User': User, 'Right': Right }
+    return { 'sa': sa, 'so': so, 'db': db, 'User': User, 'Right': Right, 'CalendarEntry': CalendarEntry }
