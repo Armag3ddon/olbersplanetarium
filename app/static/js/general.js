c7a9c9461ae1fb6c7c3da3d30c1c6cc9d76b837d
armag3ddon <armag3ddon@web.de>
2025-03-03 11:57:38 +0100

    Add generic functions script.

---
+++ app/static/js/general.js
@@ -0,0 +1,22 @@
+/*
+
+Some helper functions
+
+*/
+
+const entityMap = {
+	'&': '&amp;',
+	'<': '&lt;',
+	'>': '&gt;',
+	'"': '&quot;',
+	"'": '&#39;',
+	'/': '&#x2F;',
+	'`': '&#x60;',
+	'=': '&#x3D;'
+};
+
+function escapeHtml (string) {
+	return String(string).replace(/[&<>"'`=\/]/g, function (s) {
+		return entityMap[s];
+	});
+}
