db90226eedeacfce549c8d21bf7924636aee1912
armag3ddon <armag3ddon@web.de>
2025-03-04 00:10:40 +0100

    Add Post to shell

--- planetarium.py
+++ planetarium.py
@@ -1,11 +1,11 @@
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import create_app, db
-from app.models import User, Right, CalendarEntry
+from app.models import User, Right, CalendarEntry, Post
 
 app = create_app()
 
 # Populate the "flask shell" command with the database connection and database models
 @app.shell_context_processor
 def make_shell_context():
-    return { 'sa': sa, 'so': so, 'db': db, 'User': User, 'Right': Right, 'CalendarEntry': CalendarEntry }
+    return { 'sa': sa, 'so': so, 'db': db, 'User': User, 'Right': Right, 'CalendarEntry': CalendarEntry, 'Post': Post }
